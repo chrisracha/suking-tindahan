429190a5495f549c1ba74e033052263653bf9162
chris samuel <cosalcedo@up.edu.ph>
2025-04-29 00:40:47 +0800

    Home page preliminaries

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
 export default function Home() {
   return (
-    <main>
-      ushdiuhsdjkh
+    <main className={`h-screen flex flex-col items-center justify-center`}>
+      <h1
+        className="mb-4 text-4xl font-bold tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white break-normal"
+        >Kumusta ka?
+      </h1>
+      <h2 className="mb-3 text-2xl tracking-tight text-amber-200">
+        How are you?
+      </h2>
     </main>
   )
 }
