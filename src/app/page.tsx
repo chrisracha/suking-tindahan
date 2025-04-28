d6749b2b1e25231b6b9990fcabb9c6e34ca4346d
chris samuel <cosalcedo@up.edu.ph>
2025-04-29 00:42:33 +0800

    Edited h1 mb

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <main className={`h-screen flex flex-col items-center justify-center`}>
       <h1
-        className="mb-4 text-4xl font-bold tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white break-normal"
+        className="mb-2 text-4xl font-bold tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white break-normal"
         >Kumusta ka?
       </h1>
       <h2 className="mb-3 text-2xl tracking-tight text-amber-200">
