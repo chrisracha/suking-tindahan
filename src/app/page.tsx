b8bc18c8b912c24beb96d79bfb5aa9b3dfc24bec
chris samuel <cosalcedo@up.edu.ph>
2025-05-08 12:59:31 +0800

    Add

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -53,8 +53,8 @@ export default function Home() {
         <div className="w-full max-w-3xl">
           <div className="p-6 md:p-8 rounded-3xl bg-gradient-to-br from-gray-900 to-gray-950 shadow-[0_0_15px_rgba(255,255,255,0.1)]">
             <div className="flex flex-col items-center">
-              <h2 className="text-4xl md:text-5xl font-bold leading-normal text-center bg-gradient-to-r from-pink-200 via-purple-200 to-indigo-200 text-transparent bg-clip-text mb-10">
-                Kumusta ka ngayon?
+              <h2 className="text-3xl md:text-4xl font-bold leading-normal text-center bg-gradient-to-r from-pink-200 via-purple-200 to-indigo-200 text-transparent bg-clip-text mb-10">
+                KUMUSTA KA NGAYON?
               </h2>
               <div className="w-full space-y-10">
                 <div className="space-y-4">
